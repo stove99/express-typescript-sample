3f9efe71cef3dce9fc03d6771adf9212d356d2fa
stove99 <stove99@gmail.com>
2019-02-15 17:51:50 +0900

    커밋

---
+++ README.md
@@ -0,0 +1,7 @@
+1. npm init -y
+2. npm i -D typescript ts-node ts-loader npm-run-all webpack nodemon webpack-cli @types/node @types/express
+3. npm i express source-map-support
+4. npx tsc --init
+5. edit tsconfig.json
+6. create webpack.config.js & edit
+7. create src dir
---
+++ src/App.ts
@@ -0,0 +1,18 @@
+import * as express from "express";
+
+class App {
+    public app: express.Application;
+
+    public static bootstrap(): App {
+        return new App();
+    }
+
+    constructor() {
+        this.app = express();
+        this.app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+            res.send("Hello world!!!");
+        });
+    }
+}
+
+export default App;
